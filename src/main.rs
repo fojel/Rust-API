5d25f46bd97a48ef7a4e6fc73dc78017807061b4
idargenio <iargenio@infosis.tech>
2024-03-02 21:36:29 -0300

    chore: main add logic for a validate and err in the validation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use actix_web::{web, App, HttpServer, Responder};
-use chrono::Utc;
+use chrono::{DateTime, Utc};
+use validator::Validate;
+use serde::Deserialize;
 
-async fn index() -> impl Responder {
-    "API to get time"
+#[derive(Debug, Validate, Deserialize)]
+struct TimeRequest {
+    #[serde(rename = "timezone")]
+    #[validate(length(min = 3))]
+    timezone: String,
 }
 
-async fn getTime() -> impl Responder {
-    let time = Utc::now();
-    time.to_string();
+async fn index() -> impl Responder {
+    "¡Hola, mundo!"
+}
+
+async fn get_time(info: web::Query<TimeRequest>) -> impl Responder {
+    if let Err(e) = info.validate() {
+        return format!("Error de validación: {:?}", e).into();
+    }
+
+    let current_time = Utc::now();
+    format!("Hora actual en {}: {}", info.timezone, current_time)
 }
 
 #[actix_web::main]
@@ -15,7 +28,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/time", web::get().to(getTime()))
+            .route("/time", web::get().to(get_time))
     })
         .bind("127.0.0.1:8080")?
         .run()
